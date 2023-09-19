b9a1903906321179a419808f56fd3826210d2420
Elisée OROUNLA <126862152+hope2001Lab@users.noreply.github.com>
2023-09-19 02:52:49 +0200

    19-09-23-2

--- src/content/Applications/Messenger/BottomBarContent.tsx
+++ src/content/Applications/Messenger/BottomBarContent.tsx
@@ -32,55 +32,7 @@ const MessageInputWrapper = styled(InputBase)(
 // const Input = styled('input')({
 //   display: 'none'
 // });
-export const askIa = async (data, e) => {
- 
-  e.preventDefault();
-  try {
-   const query = data.query
-    setchatLoad(true)
-    reset()
-    
-    // console.log(inter);
-    // alert(JSON.stringify(inter));
-    // console.log(inter.slice(-3));
-    
-    const apiUrlfull = ia_api+"chatbot?collect_name=entouragetest10&query="+query+"&json_data="+JSON.stringify(inter.slice(-3))+"&model=gpt-4&temp=0&templa=As%20a%20career%20development%20expert%2C%20you%20need%20to%20provide%20a%20helpful%20and%20professional%20answer%20to%20the%20user%27s%20question%20or%20problem.%20Add%20to%20your%20answers%20interesting%20professional%20profiles%20related%20to%20his%20question.%20Remember%20that%20you%20should%20never%20make%20up%20any%20information%20if%20you%20don%27t%20have%20a%20correct%20answer%20to%20the%20question."; // Your API URL
-    // const apiUrlfull = "http://127.0.0.1:8001/chatbot?collect_name=entouragetest10&query="+query+"&model=gpt-4&temp=0&templa=As%20a%20career%20development%20expert%2C%20you%20need%20to%20provide%20a%20helpful%20and%20professional%20answer%20to%20the%20user%27s%20question%20or%20problem.%20Add%20to%20your%20answers%20interesting%20professional%20profiles%20related%20to%20his%20question.%20Remember%20that%20you%20should%20never%20make%20up%20any%20information%20if%20you%20don%27t%20have%20a%20correct%20answer%20to%20the%20question."; // Your API URL
-//   const apiUrlfull = 'http://127.0.0.1:8001/chatbot?'+'collect_name:=entouragetest10'+'&query=Bonjour'+'&json_data=%5B%5D'+'&model=gpt-3.5-turbo&temp=0.6'+'&templa='+"As a career development expert, you need to provide a helpful and professional response to the user\'s question or problem. Add to your answers interesting professional profiles related to his question. Remember that you should never invent or provide professional profiles that are not in the CONTEXT provided."; // Your API URL
-    const response = await Axios.post(apiUrlfull);
-    const res =  await ChatRequest.ask1(data.query, JSON.stringify(response.data))
-    console.log("res data");
-    console.log(res);
 
-
-    
-    setConverse([...converse,{who: "human", data: query}, {who: "ia", data: response.data} ])
-    setinter([...inter,{"query":query,"response":response.data.bot_response}])
-
-    // const res =  await ChatRequest.ask1(data.query, JSON.stringify(response.data))
-    // // const res =  await ChatRequest.ask(thisuser.id, data.query, JSON.stringify(response.data))
-    // console.log("res data");
-    // console.log(res);
-
-    setchatLoad(false)
-    reset()
-    setError(null); // Clear any previous errors
-  } catch (err) {
-    toast("Something went wrong server error",{
-      type: "error",
-      autoClose: 5000,
-      position: "top-right",
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    })
-    setchatLoad(false)
-    setError(err.message); // Handle errors
-    setResponse(null); // Clear response data on error
-  }
-};
 
 
 function BottomBarContent({ converse, setConverse, chatLoad, setchatLoad, response, setResponse, error, setError,inter, setinter}) {
