52a28528b04791c61f133d4b6d1f2cedae2c7886
Elisée OROUNLA <126862152+hope2001Lab@users.noreply.github.com>
2023-09-19 20:53:59 +0200

    commit

--- src/Services/enviro.js
+++ src/Services/enviro.js
@@ -3,7 +3,7 @@ export const back_api= "https://ec-chatbot-ai.letsdev.site/"
 // export const back_api= "http://localhost:8000/"
 export const ia_api= "https://ec-chatbot-en.letsdev.site/"
 // export const ia_api= "http://localhost:8001/"
-
+// dfdfd
 export const envi = {
     back_api, ia_api
 }
