ce61509da3469b4a256579a09a6864c943333fb4
Elisée OROUNLA <126862152+hope2001Lab@users.noreply.github.com>
2023-09-19 21:07:04 +0200

    envjs

---
+++ env.js
@@ -0,0 +1,9 @@
+export const back_api= "https://ec-chatbot-ai.letsdev.site/"
+// export const back_api= "http://localhost:8000/"
+// export const back_api= "http://localhost:8000/"
+export const ia_api= "https://ec-chatbot-en.letsdev.site/"
+// export const ia_api= "http://localhost:8001/"
+// dfdfd
+export const envi = {
+    back_api, ia_api
+}
